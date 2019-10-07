c2ffb1389fd15d73e4d7d96f7163418dc18827a7
V_Fugere <vincent.fugere@mail.mcgill.ca>
2019-10-07 13:01:06 -0400

    a custom bxp function

---
+++ mybxp.R
@@ -0,0 +1,4 @@
+mybxp <- function(x,y,colvec,ylab){
+  boxplot(y~x, lty=1, staplewex=0, boxwex=0.6, boxlwd=1, medlwd=1, col=colvec, ylab=ylab)
+  points(x=jitter(as.numeric(x)),y=y, pch=16,cex=0.5, col=alpha(1,0.3))
+}
