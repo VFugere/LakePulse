6471edc5191a248bd4c0b63587c734ab18f09b95
V_Fugere <vincent.fugere@mail.mcgill.ca>
2019-10-18 08:40:48 -0400

    some more options for bxp

--- custom_plots.R
+++ custom_plots.R
@@ -1,5 +1,5 @@
-mybxp <- function(x,y,colvec,ylab){
-  boxplot(y~x, lty=1, staplewex=0, whisklwd=1,boxwex=0.6, outline=F,boxlwd=1, medlwd=1, col=colvec, ylab=ylab)
+mybxp <- function(x,y,colvec,ylab,ylims,xlab){
+  boxplot(y~x, lty=1, staplewex=0, whisklwd=1,boxwex=0.6, xlab=xlab, outline=F,boxlwd=1, medlwd=1, col=colvec, ylab=ylab, ylim=ylims)
   points(x=jitter(as.numeric(x)),y=y, pch=16,cex=0.5, col=alpha(1,0.3))
 }
 
