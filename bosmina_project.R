46dc6805adfa988ed56acb3f4cedf15881944492
V_Fugere <vincent.fugere@mail.mcgill.ca>
2020-01-09 16:43:03 -0500

    started working on an equal area projection

--- bosmina_project.R
+++ bosmina_project.R
@@ -48,8 +48,38 @@ zooLP <- bind_rows(d2017,d2018)
 zooLP[is.na(zooLP)] <- 0
 zooLP <- zooLP[,c(1,order(names(zooLP[,2:ncol(zooLP)]))+1)]
 
+# species total biomass
+
 colSums(zooLP[,2:100]) %>% sort
+
+# number of lakes occupied
+
+occupancy <- zooLP[,2:ncol(zooLP)]
+occupancy[occupancy > 0] <- 1
+occupancy <- select(zooLP, Lake_ID) %>% bind_cols(occupancy)
+
+sort((colSums(occupancy[,2:ncol(occupancy)])/nrow(occupancy))*100)
 
 #make a map
 
+library(rgdal)
+map <- getMap(resolution = "low")
 
+map <- getMap(resolution = 'low')[which(getMap()$ADMIN %in% c('Canada','United States of America')),]
+map <- spTransform(map, CRS=CRS("+proj=aea +lat_1=26.605547828888216 +lat_2=62.451651032950174 +lon_0=-96.85546875"))
+plot(map)  
+
+load('/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/basic_data.RData')
+
+invaded <- zooLP %>% select(Lake_ID, `Eubosmina coregoni`) %>% filter(`Eubosmina coregoni` > 0)
+merged <- inner_join(basic.data,invaded, by = 'Lake_ID')
+
+col <- 'coral3'
+
+x <- basic.data$longitude
+y <- basic.data$latitude
+xrange <- range(x)+c(-2,2)
+yrange <- range(y)+c(-1,1)
+plot(map, xlim = xrange, ylim = yrange,col='light gray',border=0,asp=1,axes=F,cex.lab=0.5)
+points(x=x,y=y,pch=1,col=1,cex=1.2)
+points(x=merged$longitude,y=merged$latitude,pch=16,col=alpha(col,0.5),cex=1.1)
