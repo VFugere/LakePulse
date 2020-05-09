6c6adeaceb310d7ad333383dff489dc0e59141f0
V_Fugere <vincent.fugere@mail.mcgill.ca>
2020-05-09 10:49:53 -0400

    formats all community matrix from Annick and export as a canada-wide matrix

--- format fish.R
+++ format fish.R
@@ -14,54 +14,129 @@ comma2dot <- function(x){as.numeric(str_replace(x,',','.'))}
 return1stval <- function(x){x[1]}
 remove_empty_cols <- function(x) {return(!(all(is.na(x)) | all(x == "")))}
 
-## quebec
+# The script below formats Qc & On fish matrices. This outputs the file 'clean_community_matrix' that I sent to Annick, and 
+# which she incorporated in her excel book with all provincial sheets
 
-codes <- read_xlsx('/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/taxonomic_codes.xlsx', sheet='QC')
-Qc <- read_xlsx('/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Quebec_MFFP.xlsx', sheet=1)
-Qc <- select(Qc, Lake_ID, AMNE:SESP) %>% select_if(remove_empty_cols)
-Qc <- Qc %>% replace(is.na(.), 0)
-Qc <- as.data.frame(Qc)
-#nb.species <- ncol(Qc)-1
-Qc[,2:47][Qc[,2:47]>=1] <- 1 #converts to p/a
+# ## quebec
+# 
+# codes <- read_xlsx('/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/taxonomic_codes.xlsx', sheet='QC')
+# Qc <- read_xlsx('/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Quebec_MFFP.xlsx', sheet=1)
+# Qc <- select(Qc, Lake_ID, AMNE:SESP) %>% select_if(remove_empty_cols)
+# Qc <- Qc %>% replace(is.na(.), 0)
+# Qc <- as.data.frame(Qc)
+# #nb.species <- ncol(Qc)-1
+# Qc[,2:47][Qc[,2:47]>=1] <- 1 #converts to p/a
+# 
+# #remove bad names, change codes to binomial
+# species <- colnames(Qc)
+# species <- species[-1]
+# species.idx <- codes$binomial[match(species,codes$code)] %>% is.na
+# good.species <- species[!species.idx]
+# bad.species <- species[species.idx]
+# Qc <- select(Qc, -bad.species)
+# colnames(Qc)[2:44] <- codes$binomial[match(good.species,codes$code)]
+# 
+# ## Ontario
+# 
+# codes <- read_xlsx('/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/taxonomic_codes.xlsx', sheet='ON')
+# On <- read_xlsx('/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Ontario_BsM.xlsx', sheet=1)
+# On <- select(On, LP.ID, `longnose gar`:`spoonhead sculpin`) %>% select_if(remove_empty_cols)
+# On <- On %>% replace(is.na(.), 0)
+# colnames(On)[1] <- 'Lake_ID'
+# colnames(On)[44] <- 'mottled sculpin'
+# On <- as.data.frame(On)
+# 
+# #remove bad names, change codes to binomial
+# species <- colnames(On)
+# species <- species[-1]
+# species.idx <- codes$binomial[match(species,codes$english)] %>% is.na
+# good.species <- species[!species.idx]
+# bad.species <- species[species.idx]
+# On <- select(On, -bad.species)
+# colnames(On)[2:44] <- codes$binomial[match(good.species,codes$english)]
+# 
+# # verifying that names match
+# 
+# allfish <- bind_rows(Qc,On)
+# colnames(allfish)[2:63] %>% sort #looks good except for cyprinidae.sp
+# allfish <- select(allfish, -Cyprinidae_sp.)
+# allfish <- allfish %>% replace(is.na(.), 0)
+# 
+# com <- allfish[,2:62]
+# com <- com[,order(colnames(com))]
+# 
+# allfish <- cbind(allfish$Lake_ID,com)
+# 
+# write_xlsx(allfish, '/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/clean_community_matrix.xlsx')
 
-#remove bad names, change codes to binomial
-species <- colnames(Qc)
-species <- species[-1]
-species.idx <- codes$binomial[match(species,codes$code)] %>% is.na
-good.species <- species[!species.idx]
-bad.species <- species[species.idx]
-Qc <- select(Qc, -bad.species)
-colnames(Qc)[2:44] <- codes$binomial[match(good.species,codes$code)]
+#load other basic data
 
-## Ontario
+load(file='~/Google Drive/Recherche/Lake Pulse Postdoc/R/FisHab_Git/formatted_open_data/species_codes.RData')
+load(file='~/Google Drive/Recherche/Lake Pulse Postdoc/R/FisHab_Git/formatted_open_data/fishbase.RData')
 
-codes <- read_xlsx('/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/taxonomic_codes.xlsx', sheet='ON')
-On <- read_xlsx('/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Ontario_BsM.xlsx', sheet=1)
-On <- select(On, LP.ID, `longnose gar`:`spoonhead sculpin`) %>% select_if(remove_empty_cols)
-On <- On %>% replace(is.na(.), 0)
-colnames(On)[1] <- 'Lake_ID'
-colnames(On)[44] <- 'mottled sculpin'
-On <- as.data.frame(On)
+#load spreadsheets compiled by Annick, where she cleaned up the taxonomy for all provinces.
 
-#remove bad names, change codes to binomial
-species <- colnames(On)
-species <- species[-1]
-species.idx <- codes$binomial[match(species,codes$english)] %>% is.na
-good.species <- species[!species.idx]
-bad.species <- species[species.idx]
-On <- select(On, -bad.species)
-colnames(On)[2:44] <- codes$binomial[match(good.species,codes$english)]
+AL <- read_xlsx('~/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Annick_workbook.xlsx', sheet='AL') %>%
+  select(-data_source) %>% gather(fish_species_ID,presence, FS351:FS264) %>% arrange(id_lakepulse)
 
-# verifying that names match
+BC1 <- read_xlsx('~/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Annick_workbook.xlsx', sheet='BC-FishObs') %>%
+  select(-data_source) %>% mutate_at(vars(-id_lakepulse), as.numeric) %>% gather(fish_species_ID,presence, FS005:FS360) %>% arrange(id_lakepulse)
 
-allfish <- bind_rows(Qc,On)
-colnames(allfish)[2:63] %>% sort #looks good except for cyprinidae.sp
-allfish <- select(allfish, -Cyprinidae_sp.)
-allfish <- allfish %>% replace(is.na(.), 0)
+BC2 <- read_xlsx('~/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Annick_workbook.xlsx', sheet='BC-FishPres') %>%
+  select(-data_source) %>% mutate_at(vars(-id_lakepulse), as.numeric) %>% gather(fish_species_ID,presence, FS176:FS061) %>% arrange(id_lakepulse)
 
-com <- allfish[,2:62]
-com <- com[,order(colnames(com))]
+NB <- read_xlsx('~/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Annick_workbook.xlsx', sheet='NB') %>%
+  select(-data_source, -Smelt) %>% gather(fish_species_ID,presence, FS009:FS264) %>% arrange(id_lakepulse)
 
-allfish <- cbind(allfish$Lake_ID,com)
+NS1 <- read_xlsx('~/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Annick_workbook.xlsx', sheet='NS-FishCap') %>%
+  select(-data_source) %>% mutate_at(vars(-id_lakepulse), as.numeric) %>% gather(fish_species_ID,presence, FS009:FS264) %>% arrange(id_lakepulse)
 
-write_xlsx(allfish, '/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/clean_community_matrix.xlsx')
+NS2 <- read_xlsx('~/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Annick_workbook.xlsx', sheet='NS-Stock') %>%
+  select(-data_source) %>% mutate_at(vars(-id_lakepulse), as.numeric) %>% gather(fish_species_ID,presence, FS327:FS252) %>% arrange(id_lakepulse)
+
+QC1 <- read_xlsx('~/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Annick_workbook.xlsx', sheet='QC-PE') %>%
+  select(-data_source) %>% gather(fish_species_ID,presence, FS014:FS346) %>% arrange(id_lakepulse)
+
+ON1 <- read_xlsx('~/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Annick_workbook.xlsx', sheet='ON-MNRF') %>%
+  select(-data_source) %>% mutate_at(vars(-id_lakepulse), as.numeric) %>% gather(fish_species_ID,presence, FS010:FS342) %>% arrange(id_lakepulse)
+
+ON2 <- read_xlsx('~/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Annick_workbook.xlsx', sheet='ON-MPO') %>%
+  select(-data_source, -Centrarchidae) %>% mutate_at(vars(-id_lakepulse), as.numeric) %>% gather(fish_species_ID,presence, FS014:FS289) %>% arrange(id_lakepulse)
+
+PEI <- read_xlsx('~/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Annick_workbook.xlsx', sheet='PEI') %>%
+  select(-data_source) %>% mutate_at(vars(-id_lakepulse), as.numeric) %>% gather(fish_species_ID,presence, FS121:FS198) %>% arrange(id_lakepulse)
+
+SK <- read_xlsx('~/Google Drive/Recherche/Lake Pulse Postdoc/data/LP/fish_output/Annick_workbook.xlsx', sheet='SK') %>%
+  select(-data_source) %>% mutate_at(vars(-id_lakepulse), as.numeric) %>% gather(fish_species_ID,presence, FS264:FS327) %>% arrange(id_lakepulse)
+
+dat <- bind_rows(AL, BC1, BC2, NB, NS1, NS2, QC1, ON1, ON2, PEI, SK)
+
+dat <- dat %>% group_by(id_lakepulse,fish_species_ID) %>% summarize(pres = max(presence)) %>% ungroup
+sum(dat$fish_species_ID %in% species_codes$fish_species_ID)/nrow(dat) #all codes in there. phew!
+
+LP.fish.long <- dat
+LP.fish.wide <- dat %>% spread(fish_species_ID,pres)
+LP.fish.wide <- LP.fish.wide %>% replace(is.na(.), 0)
+
+#pour Cindy: TL
+
+dat$species <- species_codes$clean.species.name[match(dat$fish_species_ID,species_codes$fish_species_ID)]
+#only keeping stuff id'd to species
+dat <- drop_na(dat)
+#merging species codes that are the same species by grouping by name instead of FS code
+dat <- dat %>% group_by(id_lakepulse,species) %>% summarize(pres = max(pres)) %>% ungroup
+#to wide
+dat.w <- dat %>% spread(species,pres)
+dat.w <- dat.w %>% replace(is.na(.), 0)
+#adding trophic level
+for(co in 2:ncol(dat.w)){
+  sp <- colnames(dat.w)[co]
+  sp <- str_replace(sp, '_', ' ')
+  tl <- estimate.df %>% filter(Species == sp) %>% pull(Troph)
+  dat.w[,co] <- dat.w[,co]*tl
+}
+
+dat.w$max.trophic.level <- apply(dat.w[,2:ncol(dat.w)], 1, max)
+
+cindy <- select(dat.w, id_lakepulse, max.trophic.level) %>% filter(max.trophic.level > 0)
+writexl::write_xlsx(cindy, '~/Desktop/Cindy.xlsx')
